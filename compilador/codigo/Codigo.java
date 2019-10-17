9d72881ac7bd2117b65bf6218a3edb7210a4ce74
Alex Junior Pereira <alexjp17@gmail.com>
2019-10-17 20:54:35 -0300

    arrumado o conflito que foi resolvido errado, separado a classe Variable

--- compilador/codigo/Codigo.java
+++ compilador/codigo/Codigo.java
@@ -217,24 +217,6 @@ public class Codigo
 	}
 }
 
-class Variable{
-	private String id;
-	private int type;
-
-	public Variable(String id, int type){
-		this.id = id; 
-		this.type= type; 
-	}
-	
-	public String getId(){
-		return id;
-	}
-	
-	public int getType(){
-		return type; 
-	}
-}
-
 /*
 - expressoes com string so podem fazer soma
 - verificar se a funcao ja foi declarada e se o numero de 
