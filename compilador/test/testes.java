2e0e112ccd9a60a9e1266c7c69881a14fd863453
Alex Junior Pereira <alexjp17@gmail.com>
2019-10-18 17:47:10 -0300

    adicionado pocibilidade de usar aspas em comentario

--- compilador/test/testes.java
+++ compilador/test/testes.java
@@ -6,25 +6,6 @@ import java.util.ListIterator;
 
 public class testes{
     public static void main(String[] args){
-        List<Integer> exp = new ArrayList<Integer>();
-        int elem, previous=-1, next=-1;
-
-        for(int i=35;i<50;i++){
-            exp.add(i);
-        }
-        ListIterator<Integer> litr = exp.listIterator();
-
-        while(litr.hasNext()){
-            elem = litr.next();
-            if(elem==41){
-                litr.remove();
-                litr.next();
-                litr.set(4);
-            }
-        }
-
-        for(int aaaa : exp){
-            System.out.println(aaaa);
-        }
+        boolean a = "a"+4>3+5+"asd";
     }
 }
