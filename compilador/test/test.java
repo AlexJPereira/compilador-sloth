3e5855521a87cfb505133a3e259a14ce55310bd3
Alex Junior Pereira <alexjp17@gmail.com>
2019-10-28 00:48:45 -0300

    arrumado o constants adapter com o token de igualdade escrito errado

--- compilador/test/test.java
+++ compilador/test/test.java
@@ -1,10 +1,9 @@
 public class test{
-	private static double c(char d){
-		for(int i = 0; i<3; i=i+1){
-			d = d+i;
-		}
-		return d;
-	}
 	public static void main(String[] args){
+		int a = 3*2%3/4;
+		double b = 'c'+a*Math.pow(3,4);
+		/* lembrando que % e porcentagem mesmo */
+		boolean c = (2>b) && Math.pow((4*5/2),(0.03+4))%(0.03/2)<=Math.pow(5,2);
+		System.out.print(c+"\n");
 	}
 }
